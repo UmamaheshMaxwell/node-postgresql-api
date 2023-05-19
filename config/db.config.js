aba74b6e969f12767b3d8ea75b9033cfaca1a640
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-05-20 02:04:54 +0530

    modified db config file

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = () => {
 	return {
-		HOST:  process.ENV.DB_HOST || "1.2.3.4",
-		PORT: process.ENV.DB_PORT || "5432",
-		PASSWORD: process.ENV.DB_PASSWORD || "pass",
-		DB: process.ENV.DB_NAME || "db",
-		DBUser: process.ENV.DB_USER || "user"
+		HOST:  process.env.DB_HOST || "34.136.17.61",
+		PORT: process.env.DB_PORT || "5432",
+		PASSWORD: process.env.DB_PASSWORD || "user@123",
+		DB: process.env.DB_NAME || "userdb",
+		DBUser: process.env.DB_USER || "postgres"
 	}
 };
