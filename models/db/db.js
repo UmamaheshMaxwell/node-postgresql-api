125bb87f202b40b87df4ce826b91dd28c7157b25
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-05-20 16:48:50 +0530

    modified config file

--- models/db/db.js
+++ models/db/db.js
@@ -2,7 +2,22 @@ const DBConfig = require('../../config/db.config')()
 const Sequelize = require('sequelize');
 const db = {}
 
-let config = { dialect: 'postgres', host: DBConfig.HOST }
+let config = { 
+        dialect: 'postgres', 
+        host: DBConfig.HOST,
+        pool: {
+            max: 22,
+            min: 1,
+            acquire: 600000,
+            idle: 100000,
+        },
+        createdAt: false,
+        updatedAt: false,
+        timestamp: false,
+        dialectOptions: {
+            connectTimeout: 3000000,
+        }
+}
 
 const sequelize = new Sequelize(DBConfig.DB, DBConfig.DBUser, DBConfig.PASSWORD, config);
 try {
