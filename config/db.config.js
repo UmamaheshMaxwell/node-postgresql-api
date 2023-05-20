5d246047a0f7f6ced13a3aab2b01043771cb9bb2
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-05-20 11:51:19 +0530

    modified cloud build

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ module.exports = () => {
 		HOST:  process.env.DB_HOST || "34.136.17.61",
 		PORT: process.env.DB_PORT || "5432",
 		PASSWORD: process.env.DB_PASSWORD || "user@123",
-		DB: process.env.DB_NAME || "userdb",
+		DB: process.env.DB_DATABASE || "userdb",
 		DBUser: process.env.DB_USER || "postgres"
 	}
 };
